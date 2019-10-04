109d56b64fc6e671cc54854022154a9e1f757fb6
shee1998 <marywanjikumburu1@gmail.com>
2019-10-04 14:32:29 +0300

    update html divisions sector by adding content, style content on styless.css

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//shows jquery function for menu
+
+$('#pizza1').mouseenter(function () {
+  $('.pizza-heading').filter('#pizza1-title').show("slow");
+}).mouseleave(function () {
+  $('#pizza1-title').hide("slow");
+});
+
+$('#pizza2').mouseenter(function () {
+  $('.pizza-heading').filter('#pizza2-title').show("slow");
+}).mouseleave(function () {
+  $('#pizza2-title').hide("slow");
+});
+
+
+$('#pizza3').mouseenter(function () {
+  $('.pizza-heading').filter('#pizza3-title').show("slow");
+}).mouseleave(function () {
+  $('#pizza3-title').hide("slow");
+});
+
+$('#pizza4').mouseenter(function () {
+  $('.pizza-heading').filter('#pizza4-title').show("slow");
+}).mouseleave(function () {
+  $('#pizza4-title').hide("slow");
+});
+
+
+$('#pizza5').mouseenter(function () {
+  $('.pizza-heading').filter('pizza5-title').show("slow");
+}).mouseleave(function () {
+  $('#pizza5-title').hide("slow");
+});
+
+$('#pizza6').mouseenter(function(){
+$('.pizza-heading').filter('#pizza6-title').show("slow");
+}).mouseleave(function(){
+$('#pizza6-title').hide("slow");
+});
