8d3f81835502880e7de877c0cfd10ba320f161cb
shee1998 <marywanjikumburu1@gmail.com>
2019-10-07 08:54:32 +0300

    add cart function to script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
-var crust,toppings,size, price, total;
+var crust, toppings, size, price, total;
 function get(size, crust, toppings, total) {
-    this.size = size;
-    this.crust = crust;
-    this.toppings = toppings;
-    this.total = total;
+  this.size = size;
+  this.crust = crust;
+  this.toppings = toppings;
+  this.total = total;
 }
 
-$(document).ready(function(){
-  $('button#Place your Order').click(function(){
-      var pizzaSize = $('#pizzaSize:chosen').val();
-      var pizzaCrust = $('#pizzaCrust:chosen').val();
+$(document).ready(function () {
+  $('button#placeOrder').click(function () {
+    var pizzaSize = $('#pizzaSize:chosen').val();
+    var pizzaCrust = $('#pizzaCrust:chosen').val();
 
-      var pizzaTopping = [];
-      
-      $.each($("input[for='topping']:chosen"), function(){
-          pTopping.push($(this).val());
-      });
-      var pizzaTopping = pTopping.length*80;
-      var pizzaTotal = parseInt(pizzaSize) + parseInt(pizzaCrust)+ parseInt(pizzaTopping);
-      alert("Your total order is: "+pizzaTotal)
-      $('#location').show();
-      event.preventDefault();
+    var pizzaTopping = [];
+
+    $('#cart').click(function () {
+      alert(pizzaSize)
+    })
+
+    $.each($("input[for='topping']:chosen"), function () {
+      pTopping.push($(this).val());
+    });
+    var pizzaTopping = pTopping.length * 80;
+    var pizzaTotal = parseInt(pizzaSize) + parseInt(pizzaCrust) + parseInt(pizzaTopping);
+    alert("Your total order is: " + pizzaTotal)
+    $('#location').show();
+    event.preventDefault();
   });
 });
 
@@ -41,43 +45,43 @@ $(document).ready(function(){
 
 //shows jquery function for menu
 
-$(document).ready(function(){
+$(document).ready(function () {
   $('#pizza1').mouseenter(function () {
     $('.pizza-heading').filter('#pizza1-title').show("slow");
   }).mouseleave(function () {
     $('#pizza1-title').hide("slow");
   });
-  
+
   $('#pizza2').mouseenter(function () {
     $('.pizza-heading').filter('#pizza2-title').show("slow");
   }).mouseleave(function () {
     $('#pizza2-title').hide("slow");
   });
-  
+
   $('#pizza3').mouseenter(function () {
     $('.pizza-heading').filter('#pizza3-title').show("slow");
   }).mouseleave(function () {
     $('#pizza3-title').hide("slow");
   });
-  
+
   $('#pizza4').mouseenter(function () {
     $('.pizza-heading').filter('#pizza4-title').show("slow");
   }).mouseleave(function () {
     $('#pizza4-title').hide("slow");
   });
-  
-  $('#viewCart').click(function(){
+
+  $('#viewCart').click(function () {
     $('#orderPizza').modal('hide');
   });
-  $('#addMore').click(function(){
+  $('#addMore').click(function () {
     $('#cartPizza').modal('hide');
     $('#orderPizza').modal('show');
   });
-  $('#checkout').click(function(){
+  $('#checkout').click(function () {
     let location = $('#location').val();
     $('#cartPizza').modal('hide');
     alert("Your order will be delivered to " + location);
-   
+
   });
 
 })
